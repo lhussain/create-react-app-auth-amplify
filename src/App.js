974e4f706c500cda35975b52aabed57092be75b4
lomanhussain <loman.hussain@blackcatsolutions.com>
2020-10-22 16:25:41 +0100

    added cognito user pool details

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+Amplify.configure({
+  "aws_project_region": "eu-west-1",
+  "aws_cognito_identity_pool_id": "eu-west-1:00852205-c605-4ad1-8d1d-e0d00c184df0",
+  "aws_cognito_region": "eu-west-1",
+  "aws_user_pools_id": "eu-west-1_kD4fI57R5",
+  "aws_user_pools_web_client_id": "5s439moh8i6kg39oot0q19t8mp",
+});
 
 class App extends Component {
   render() {
