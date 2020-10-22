1b5aee9109bdf8b290f950e4db0926030a00de26
lomanhussain <loman.hussain@blackcatsolutions.com>
2020-10-22 16:38:59 +0100

    change app client to use no secret

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ Amplify.configure({
   "aws_cognito_identity_pool_id": "eu-west-1:00852205-c605-4ad1-8d1d-e0d00c184df0",
   "aws_cognito_region": "eu-west-1",
   "aws_user_pools_id": "eu-west-1_kD4fI57R5",
-  "aws_user_pools_web_client_id": "5s439moh8i6kg39oot0q19t8mp",
+  "aws_user_pools_web_client_id": "7eeli2g30kmjplsp5669he0lva",
 });
 
 class App extends Component {
